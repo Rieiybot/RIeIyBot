fc9a83e11d7ebc63c4b0b0a6cab786642b0d0ce1
Rieiybot <155292910+Rieiybot@users.noreply.github.com>
2023-12-31 18:11:34 +0200

    Update start&endcall.py

--- AnonXMusic/plugins/play/start&endcall.py
+++ AnonXMusic/plugins/play/start&endcall.py
@@ -15,16 +15,16 @@ async def brah2(client, message):
        await message.reply("• قفله ف دماغك يبعيد 🙄🖤")
 @app.on_message(filters.video_chat_members_invited)
 async def fuckoff(client, message):
-           text = f"• الراجل دا عايزك ف الكول ← {message.from_user.mention}\n"
+           text = f"• الراجل دا عايزك ف الكول ← {message.from_user.mention}"
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-              text += f"\n◈ {user.first_name}"
+              text += f"\n│\n╰⦿ [{user.first_name}](tg://user?id={user.id})"
               x = 1
              except Exception:
                pass
            try:
-             await message.reply(f"{text}", parse_mode = ParseMode.MARKDOWN)
+             await message.reply(f"{text}")
            except:
              pass
 
