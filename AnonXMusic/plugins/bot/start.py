f283fa085b9a1cefc9cb20a044ab0a50a4267f51
Rieiybot <155292910+Rieiybot@users.noreply.github.com>
2024-01-01 19:30:21 +0200

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -46,7 +46,7 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("※ انتظر جاري الشتغيل ...")
+            m = await message.reply_text("⚡️")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
