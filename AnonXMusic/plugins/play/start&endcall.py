6cb37e5c17530bd7d47160f01a5fdd80bc043abb
Rieiybot <155292910+Rieiybot@users.noreply.github.com>
2024-01-02 13:29:06 +0200

    Update start&endcall.py

--- AnonXMusic/plugins/play/start&endcall.py
+++ AnonXMusic/plugins/play/start&endcall.py
@@ -7,24 +7,26 @@ from pyrogram.enums import ParseMode
 
 
 
-@app.on_message(filters.video_chat_started)
-async def brah(client, message):
-       await message.reply("• عندي برد مش قادر اغني 😪🖤")
-@app.on_message(filters.video_chat_ended)
-async def brah2(client, message):
-       await message.reply("• قفله ف دماغك يبعيد 🙄🖤")
-@app.on_message(filters.video_chat_members_invited)
-async def fuckoff(client, message):
-           text = f"• الراجل دا عايزك ف الكول ← {message.from_user.mention}"
-           x = 0
-           for user in message.video_chat_members_invited.users:
-             try:
-              text += f"\n│\n╰⦿ [{user.first_name}](tg://user?id={user.id})"
-              x = 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text}")
-           except:
-             pass
+@Client.on_message(filters.video_chat_started)
+async def brah(client: Client, message):
+    await message.reply("**♪ تم بدا المحادثه الصوتيه 💎 .**")
+
+@Client.on_message(filters.video_chat_ended)
+async def brah2(client: Client, message):
+    await message.reply("**♪ تم انهاء المحادثه الصوتيه 💎 .**")
+
+@Client.on_message(filters.video_chat_members_invited)
+async def fuckoff(client: Client, message):
+    text = f"♪ قام : {message.from_user.mention} 💎 .\n"
+    x = 0
+    for user in m.video_chat_members_invited.users:
+        try:
+            text += f"♪ بدعوة -> {user.mention} 💎 .\n♪ إلي المحادثة المرئية 💎 ."
+            x += 1
+        except Exception:
+            pass
+    try:
+        await m.reply(f"**{text}**", reply_to_message_id=m.message_id)
+    except:
+        pass
 
