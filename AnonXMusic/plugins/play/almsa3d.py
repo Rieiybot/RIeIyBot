54f4ad85f53c3684a6358c25280ce6aa54fc6e61
Rieiybot <155292910+Rieiybot@users.noreply.github.com>
2024-01-03 21:12:52 +0200

    Update almsa3d.py

--- AnonXMusic/plugins/play/almsa3d.py
+++ AnonXMusic/plugins/play/almsa3d.py
@@ -4,38 +4,38 @@ from pyrogram.types import Message
 from AnonXMusic import ASS_MENTION, LOGGER, SUDOERS, app, app2
 
 
-@app.on_message(filters.command(["asspfp", "setpfp"]) & SUDOERS)
+@app.on_message(filters.command(["asspfp", "setpfp","تغيير صورة المساعد"], "") & SUDOERS)
 async def set_pfp(_, message: Message):
     if message.reply_to_message.photo:
-        fuk = await message.reply_text("» ᴄʜᴀɴɢɪɴɢ ᴀssɪsᴛᴀɴᴛ's ᴘʀᴏғɪʟᴇ ᴘɪᴄ...")
+        fuk = await message.reply_text("» جاري تغيير صورة المساعد...")
         img = await message.reply_to_message.download()
         try:
             await app2.set_profile_photo(photo=img)
             return await fuk.edit_text(
-                f"» {ASS_MENTION} ᴘʀᴏғɪʟᴇ ᴘɪᴄ ᴄʜᴀɴɢᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ."
+                f"» تم تغيير صورة المساعد \n│\n╰⦿ᚐ{ASS_MENTION}."
             )
         except:
-            return await fuk.edit_text("» ғᴀɪʟᴇᴅ ᴛᴏ ᴄʜᴀɴɢᴇ ᴀssɪsᴛᴀɴᴛ's ᴘʀᴏғɪʟᴇ ᴘɪᴄ.")
+            return await fuk.edit_text("» فشل تغيير الصوره.")
     else:
         await message.reply_text(
-            "» ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ғᴏʀ ᴄʜᴀɴɢɪɴɢ ᴀssɪsᴛᴀɴᴛ's ᴘʀᴏғɪʟᴇ ᴘɪᴄ."
+            "» الرجاء الرد علي الصوره المراد وضعها."
         )
 
 
-@app.on_message(filters.command(["delpfp", "delasspfp"]) & SUDOERS)
+@app.on_message(filters.command(["delpfp", "delasspfp","حذف صورة المساعد"], "") & SUDOERS)
 async def set_pfp(_, message: Message):
     try:
         pfp = [p async for p in app2.get_chat_photos("me")]
         await app2.delete_profile_photos(pfp[0].file_id)
         return await message.reply_text(
-            "» sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ᴀssɪsᴛᴀɴᴛ's ᴘʀᴏғɪʟᴇ ᴘɪᴄ."
+            "» تم حذف صورة المساعد بنجاح."
         )
     except Exception as ex:
         LOGGER.error(ex)
-        await message.reply_text("» ғᴀɪʟᴇᴅ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴀssɪsᴛᴀɴᴛ's ᴘʀᴏғɪʟᴇ ᴘɪᴄ.")
+        await message.reply_text("» فشل حذف الصوره .")
 
 
-@app.on_message(filters.command(["assbio", "setbio"]) & SUDOERS)
+@app.on_message(filters.command(["assbio", "setbio","تغيير بايو الساعد"], "") & SUDOERS)
 async def set_bio(_, message: Message):
     msg = message.reply_to_message
     if msg:
@@ -43,19 +43,19 @@ async def set_bio(_, message: Message):
             newbio = msg.text
             await app2.update_profile(bio=newbio)
             return await message.reply_text(
-                f"» {ASS_MENTION} ʙɪᴏ ᴄʜᴀɴɢᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ."
+                f"» تم تغيير بايو المساعد \n│\n╰⦿ᚐ{ASS_MENTION}"
             )
     elif len(message.command) != 1:
         newbio = message.text.split(None, 1)[1]
         await app2.update_profile(bio=newbio)
-        return await message.reply_text(f"» {ASS_MENTION} ʙɪᴏ ᴄʜᴀɴɢᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.")
+        return await message.reply_text(f"» تم تغيير بايو المساعد \n│\n╰⦿ᚐ{ASS_MENTION}")
     else:
         return await message.reply_text(
-            "» ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴛ ɪᴛ ᴀs ᴀssɪsᴛᴀɴᴛ's ʙɪᴏ."
+            "» الرجاء الرد علي البايو المراد وضعه."
         )
 
 
-@app.on_message(filters.command(["assname", "setname"]) & SUDOERS)
+@app.on_message(filters.command(["assname", "setname","تغيير اسم المساعد"], "") & SUDOERS)
 async def set_name(_, message: Message):
     msg = message.reply_to_message
     if msg:
@@ -63,13 +63,13 @@ async def set_name(_, message: Message):
             name = msg.text
             await app2.update_profile(first_name=name)
             return await message.reply_text(
-                f"» {ASS_MENTION} ɴᴀᴍᴇ ᴄʜᴀɴɢᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ."
+                f"» تم تغيير اسم المساعد \n│\n╰⦿ᚐ{ASS_MENTION} "
             )
     elif len(message.command) != 1:
         name = message.text.split(None, 1)[1]
         await app2.update_profile(first_name=name, last_name="")
-        return await message.reply_text(f"» {ASS_MENTION} ɴᴀᴍᴇ ᴄʜᴀɴɢᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.")
+        return await message.reply_text(f"» تم تغيير اسم المساعد \n│\n╰⦿ᚐ{ASS_MENTION}")
     else:
         return await message.reply_text(
-            "» ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴛ ɪᴛ ᴀs ᴀssɪsᴛᴀɴᴛ's ɴᴇᴡ ɴᴀᴍᴇ."
+            "» الرجاء الرد علي اسم المساعد المراد وضعه."
         )
